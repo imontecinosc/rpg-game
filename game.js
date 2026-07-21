0a7676f7e58ce97b77469638efb141d89c680118
imontecinosc <ignacio.montecinos@gmail.com>
2026-07-20 23:34:33 -0400

    Update game.js

--- game.js
+++ game.js
@@ -1,1 +1,30 @@
-console.log("Juego iniciado");
+const canvas = document.getElementById("game");
+const ctx = canvas.getContext("2d");
+
+function resize(){
+
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+}
+
+window.addEventListener("resize",resize);
+
+resize();
+
+function gameLoop(){
+
+    // Fondo
+    ctx.fillStyle="#2d5a27";
+    ctx.fillRect(0,0,canvas.width,canvas.height);
+
+    // Texto
+    ctx.fillStyle="white";
+    ctx.font="30px Arial";
+    ctx.fillText("ULTIMA RPG",40,50);
+
+    requestAnimationFrame(gameLoop);
+
+}
+
+gameLoop();
