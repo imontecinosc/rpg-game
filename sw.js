616a284396d53d2d6b023081c98e69d4f0f844a3
imontecinosc <ignacio.montecinos@gmail.com>
2026-07-23 18:13:40 -0400

    Delete sw.js

--- sw.js
+++
@@ -1,4 +0,0 @@
-const CACHE='ultra-v44';const FILES=['./','./index.html','./style.css?v=440','./game.js?v=440','./manifest.json','./icon-192.png','./icon-512.png'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES))));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
