71363ef2cdfb52542f7ffbcae789f238d0fee7f4
imontecinosc <ignacio.montecinos@gmail.com>
2026-07-27 12:23:10 -0400

    Delete js directory

--- js/systems/crafting.js
+++
@@ -1,3 +0,0 @@
-(function(U){
-  U.craft=function(i){const r=U.recipes[i];if(!r||U.player.skills[r.skill]<r.lvl)return;for(const [id,q] of Object.entries(r.in))if(U.countItem(id)<q)return U.toast('Te faltan materiales.');for(const [id,q] of Object.entries(r.in))U.removeItem(id,q);for(const [id,q] of Object.entries(r.out))U.addItem(id,q);U.raiseSkill(r.skill,.35);U.toast('Fabricas '+r.name);U.ui.refreshAll();U.ui.refreshCraft(r.cat)};
-})(window.Ultra=window.Ultra||{});
